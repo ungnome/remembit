777a97109751350d503defcb4694de5c9d51237c
nevyk <73322863+nevyk@users.noreply.github.com>
2022-07-26 00:51:49 -0400

    feat: add theme support

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,31 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import TheReloadPrompt from './components/TheReloadPrompt.vue';
 import { useSettings } from './store/settings';
+import { useThemeController } from './composables/themeController';
 
 // load the user store
 const settings = useSettings();
 
+// init theme controller
+const themeController = useThemeController();
+
 // load app settings
 settings.load();
 
-// persist settings when they change
+// apply app theme
+themeController.updateTheme();
+
+// listen for changes to settings
 settings.$subscribe(() => {
+  // save new settings
   settings.save();
+
+  themeController.updateTheme();
 });
 </script>
 
 <template>
+  k
   <ion-app>
     <ion-router-outlet />
   </ion-app>
---
+++ src/composables/themeController.ts
@@ -0,0 +1,41 @@
+import { useSettings } from '../store/settings';
+
+export function useThemeController() {
+  const settings = useSettings();
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+  function handlePreferredColorChange(event: MediaQueryListEvent) {
+    console.log(event);
+    document.body.classList.toggle('dark', event.matches);
+  }
+
+  function enableSystemTheme() {
+    prefersDark.addEventListener('change', handlePreferredColorChange);
+  }
+
+  function disableSystemTheme() {
+    prefersDark.removeEventListener('change', handlePreferredColorChange);
+  }
+
+  function updateTheme() {
+    console.log('refreshing theme.');
+    switch (settings.theme) {
+      case 'light':
+        disableSystemTheme();
+        document.body.classList.remove('dark');
+        break;
+      case 'dark':
+        disableSystemTheme();
+        document.body.classList.add('dark');
+        break;
+      case 'system':
+        enableSystemTheme();
+        document.body.classList.toggle('dark', prefersDark.matches);
+        break;
+    }
+  }
+
+  return {
+    updateTheme
+  };
+}
