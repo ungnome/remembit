19c8092ad5b1791c91ae253fd495501dc1c05c78
ungnome <73322863+ungnome@users.noreply.github.com>
2024-12-26 01:02:37 -0500

    feat(web): user account page skeleton

--- web/src/lib/components/Navbar.svelte
+++ web/src/lib/components/Navbar.svelte
@@ -24,16 +24,19 @@
 	</div>
 
 	{#if $currentUser}
-		<BookmarkFormModal bind:this={bookmarkFormModal} />
-		<div class="navbar-center">
-			<input
-				class="input input-md input-bordered mx-3"
-				placeholder="Search"
-				bind:value={$searchString}
-			/>
-			<button class="btn btn-md" onclick={handleCreate}>Add <BookmarkIcon class="size-4" /></button
-			>
-		</div>
+		{#if page.url.pathname === '/bookmarks'}
+			<BookmarkFormModal bind:this={bookmarkFormModal} />
+			<div class="navbar-center">
+				<input
+					class="input input-md input-bordered mx-3"
+					placeholder="Search"
+					bind:value={$searchString}
+				/>
+				<button class="btn btn-md" onclick={handleCreate}
+					>Add <BookmarkIcon class="size-4" /></button
+				>
+			</div>
+		{/if}
 		<div class="navbar-end">
 			<div class="nav-end flex-none gap-2">
 				<div class="dropdown dropdown-end">
@@ -46,6 +49,7 @@
 						</div>
 					</div>
 					<ul class="menu dropdown-content z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
+						<li><a href="/account">Account</a></li>
 						<li><a href="/signin" onclick={handleSignout}>Sign Out</a></li>
 					</ul>
 				</div>
