73109b7b54099c7e5c534e644b764e1a49db9236
ungnome <73322863+ungnome@users.noreply.github.com>
2024-12-29 22:36:56 -0500

    feat(web): add bottom nav for small screens

--- web/src/routes/account/+page.svelte
+++ web/src/routes/account/+page.svelte
@@ -34,45 +34,39 @@
 <ChangeEmailModal bind:this={changeEmailModal} />
 <ChangePasswordModal bind:this={changePasswordModal} />
 
-<div class="grid place-items-center">
-	<div class="card w-2/4">
-		<div class="card-body">
-			<h2 class="card-title mb-4 block text-center">Account</h2>
-			<div class="join join-vertical">
-				<label for="name" class="label font-bold">Name</label>
-				<input
-					type="text"
-					class="input input-bordered w-full"
-					placeholder={$currentUser!.name}
-					bind:value={name}
-				/>
-			</div>
+<div class="grid place-items-center mx-4 sm:mx-0">
+	<div class="grid w-full sm:w-[35rem]">
+		<h2 class="justify-self-center text-xl font-bold">Account</h2>
 
-			<div class="join join-vertical">
-				<label for="email" class="label font-bold">Email</label>
-				<span class="mb-2">{$currentUser!.email}</span>
-				<button
-					onclick={() => {
-						changeEmailModal.show();
-					}}
-					class="btn max-w-xs">Change Email</button
-				>
-			</div>
+		<label for="name" class="label font-bold">Name</label>
+		<input
+			type="text"
+			class="input input-bordered"
+			placeholder={$currentUser!.name}
+			bind:value={name}
+		/>
 
-			<div class="join join-vertical">
-				<label for="change-password" class="label font-bold">Password</label>
-				<button
-					id="change-password"
-					onclick={() => {
-						changePasswordModal.show();
-					}}
-					class="btn max-w-xs">Change Password</button
-				>
-			</div>
-		</div>
+		<label for="email" class="label font-bold">Email</label>
+		<span class="mb-2">{$currentUser!.email}</span>
+		<button
+			onclick={() => {
+				changeEmailModal.show();
+			}}
+			class="btn max-w-xs">Change Email</button
+		>
+
+		<label for="change-password" class="label font-bold">Password</label>
+		<button
+			id="change-password"
+			onclick={() => {
+				changePasswordModal.show();
+			}}
+			class="btn max-w-xs">Change Password</button
+		>
 
 		<div class="divider"></div>
-		<div class="card-actions justify-between">
+
+		<div class="flex justify-between">
 			<button onclick={handleDelete} class="btn btn-outline btn-error">Delete Account</button>
 			<button onclick={handleSave} class="btn {hasChanged ? 'btn-primary' : 'btn-disabled'}"
 				>Save
@@ -80,3 +74,49 @@
 		</div>
 	</div>
 </div>
+<!-- <div class="grid place-items-center"> -->
+<!-- 	<div class="card"> -->
+<!-- 		<div class="card-body"> -->
+<!-- 			<h2 class="card-title mb-4 block text-center">Account</h2> -->
+<!-- 			<div class="join join-vertical"> -->
+<!-- 				<label for="name" class="label font-bold">Name</label> -->
+<!-- 				<input -->
+<!-- 					type="text" -->
+<!-- 					class="input input-bordered w-full" -->
+<!-- 					placeholder={$currentUser!.name} -->
+<!-- 					bind:value={name} -->
+<!-- 				/> -->
+<!-- 			</div> -->
+<!---->
+<!-- 			<div class="join join-vertical"> -->
+<!-- 				<label for="email" class="label font-bold">Email</label> -->
+<!-- 				<span class="mb-2">{$currentUser!.email}</span> -->
+<!-- 				<button -->
+<!-- 					onclick={() => { -->
+<!-- 						changeEmailModal.show(); -->
+<!-- 					}} -->
+<!-- 					class="btn max-w-xs">Change Email</button -->
+<!-- 				> -->
+<!-- 			</div> -->
+<!---->
+<!-- 			<div class="join join-vertical"> -->
+<!-- 				<label for="change-password" class="label font-bold">Password</label> -->
+<!-- 				<button -->
+<!-- 					id="change-password" -->
+<!-- 					onclick={() => { -->
+<!-- 						changePasswordModal.show(); -->
+<!-- 					}} -->
+<!-- 					class="btn max-w-xs">Change Password</button -->
+<!-- 				> -->
+<!-- 			</div> -->
+<!-- 		</div> -->
+<!---->
+<!-- 		<div class="divider"></div> -->
+<!-- 		<div class="card-actions justify-between"> -->
+<!-- 			<button onclick={handleDelete} class="btn btn-outline btn-error">Delete Account</button> -->
+<!-- 			<button onclick={handleSave} class="btn {hasChanged ? 'btn-primary' : 'btn-disabled'}" -->
+<!-- 				>Save -->
+<!-- 			</button> -->
+<!-- 		</div> -->
+<!-- 	</div> -->
+<!-- </div> -->
