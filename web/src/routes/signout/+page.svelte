eda1ac99da50a5033e488a28075b2b012deadf3a
ungnome <73322863+ungnome@users.noreply.github.com>
2024-12-31 16:30:56 -0500

    fix(web): signout page not clearing authstore

--- web/src/routes/signout/+page.svelte
+++ web/src/routes/signout/+page.svelte
@@ -1,5 +1,9 @@
-<script lang="ts" module>
+<script lang="ts">
 	import { pb } from '$lib/services/pocketbase';
 
 	pb.authStore.clear();
 </script>
+
+<div class="flex place-content-center">
+	<p>You have been signed out</p>
+</div>
