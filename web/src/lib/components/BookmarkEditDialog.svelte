b60463984513e137f726d2ae48d06c8799c770ad
ungnome <73322863+ungnome@users.noreply.github.com>
2025-04-06 18:01:07 -0400

    fix(web): bookmark dialog form layout (#49)

--- web/src/lib/components/BookmarkEditDialog.svelte
+++ web/src/lib/components/BookmarkEditDialog.svelte
@@ -13,22 +13,21 @@
   }
 
   export function show() {
-    dialogElement.show();
+    dialogElement.showModal();
   }
 </script>
 
-<dialog
-  class="daisyui-modal daisyui-modal-bottom sm:daisyui-modal-middle"
-  bind:this={dialogElement}
->
+<dialog class="daisyui-modal daisyui-modal-bottom sm:daisyui-modal-middle" bind:this={dialogElement}>
   <div class="daisyui-modal-box">
     <h3 class="mb-6 text-lg font-bold capitalize">Edit Bookmark</h3>
-    <form onsubmit={submit} action="" method="dialog">
-      <label class="daisyui-input mb-2 flex items-center gap-2">
-        <input bind:value={name} class="grow" type="text" placeholder="Name" required />
+    <form onsubmit={submit} action="" method="dialog" >
+      <label class="daisyui-floating-label mb-6 flex items-center">
+        <span>Name</span>
+        <input bind:value={name} class="daisyui-input grow" type="text" placeholder="Name" required />
       </label>
-      <label class="daisyui-input mb-2 flex items-center gap-2">
-        <input bind:value={url} class="grow" type="url" placeholder="Url" required />
+      <label class="daisyui-floating-label flex items-center">
+        <span>Url</span>
+        <input bind:value={url} class="daisyui-input grow" type="url" placeholder="Url" required />
       </label>
       <div class="daisyui-modal-action">
         <button type="submit" class="daisyui-btn">Submit</button>
