d942f50c93f1251cb2c04a8319e048e98b7ca9d4
ungnome <73322863+ungnome@users.noreply.github.com>
2025-05-18 17:19:54 -0400

    polish bookmarks page

--- web/src/routes/bookmarks/+page.svelte
+++ web/src/routes/bookmarks/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import BookmarkNewDialog from '$lib/components/BookmarkNewDialog.svelte';
+  import BookmarkListItem from '$lib/components/BookmarkListItem.svelte';
   import { onMount, type SvelteComponent } from 'svelte';
-  import bookmarks from '$lib/stores/bookmarks.svelte';
-  import BookmarkTableRow from '$lib/components/BookmarkTableRow.svelte';
+  import { bookmarks, type Bookmark } from '$lib/stores/bookmarks.svelte';
 
   onMount(() => {
     bookmarks.refresh();
@@ -19,27 +19,20 @@
 
 <BookmarkNewDialog bind:this={newBookmarkDialog} />
 
-<div class="mx-4 my-2 flex gap-4">
-  <button class="daisyui-btn" onclick={handleAdd}>Add</button>
-  <label for="" class="daisyui-floating-label flex grow items-center">
-    <span>Search</span>
-    <input type="search" placeholder="Search" class="daisyui-input grow" bind:value={bookmarks.filter} />
-  </label>
-</div>
+<div class="mx-2">
+  <div class="bg-base-100 sticky top-16 z-1 pb-2">
+    <div class="flex gap-4">
+      <button class="daisyui-btn daisyui-btn-primary" onclick={handleAdd}>Add</button>
+      <label for="" class="daisyui-floating-label flex grow items-center">
+        <span>Search</span>
+        <input type="search" placeholder="Search" class="daisyui-input grow" bind:value={bookmarks.filter} />
+      </label>
+    </div>
+  </div>
 
-<div class="h-[calc(100vh-8rem)] overflow-x-auto">
-  <table class="daisyui-table daisyui-table-pin-rows">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>URL</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each bookmarkList as bookmark (bookmark.id)}
-        <BookmarkTableRow {bookmark} />
-      {/each}
-    </tbody>
-  </table>
+  <ul class="daisyui-list bg-base-100 rounded-box shadow-none">
+    {#each bookmarkList as bookmark (bookmark.id)}
+      <BookmarkListItem {bookmark} />
+    {/each}
+  </ul>
 </div>
